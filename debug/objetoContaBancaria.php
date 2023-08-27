050435f237d0674b9b7c9c8f6effefbc797ff74c
00Theory <marco200002010@outlook.com>
2023-08-26 22:13:43 -0300

    Estudo de abstração e encapsulamento php

--- debug/objetoContaBancaria.php
+++ debug/objetoContaBancaria.php
@@ -4,6 +4,19 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use App\ContaBancaria;
 
-$contaBancaria = new ContaBancaria();
-var_dump($contaBancaria -> exibirDadosConta());
-var_dump($contaBancaria -> banco);
+$contaBancaria = new ContaBancaria(
+    'Banco do Brasil',
+    'Marco Lima',
+    '9132',
+    '1321-01',
+    '1000000'
+);
+
+
+$contaBancaria -> setBanco("Banco do Brasil");
+$contaBancaria -> setNomeTitular("Marco Lima");
+$contaBancaria -> setNumeroAgencia("9132");
+$contaBancaria -> setNumeroConta("1321-01");
+$contaBancaria -> setSaldo("1000000");
+
+var_dump($contaBancaria -> getBanco());
--- src/ContaBancaria.php
+++ src/ContaBancaria.php
@@ -5,22 +5,41 @@ declare(strict_types=1);
 namespace App;
 
 class ContaBancaria {
-    public string $banco = "Banco Cuide";
-    public string $nomeTitular = "Marco Lima";
-    public string $numeroAgencia = "8244";
-    public string $numeroConta = "39120-69";
-    public float $saldo = 0;
+    private string $banco;
+    private string $nomeTitular;
+    private string $numeroAgencia;
+    private string $numeroConta;
+    private float $saldo;
 
     public function exibirDadosConta(): array{
         return [
             'banco' => $this -> banco,
-            'nomeTitular' => $this-> nomeTitular,
+            'nomeTitular' => $this -> nomeTitular,
             'numeroAgencia' => $this -> numeroAgencia,
             'numeroconta' => $this -> numeroConta,
             'saldo' => $this -> saldo,
         ];
     }
+
+	public function getBanco(): string
+    {
+        return $this->banco;
+    }
+
+	public function getNomeTitular(): string {
+		return $this->nomeTitular;
+	}
+	
+	public function getNumeroAgencia(): string {
+		return $this->numeroAgencia;
+	}
+
+	public function getNumeroConta(): string {
+		return $this->numeroConta;
+	}
+
+	public function getSaldo(): float {
+		return $this->saldo;
+	}
 }
 
-$contaBancaria = new ContaBancaria();
-var_dump($contaBancaria);
